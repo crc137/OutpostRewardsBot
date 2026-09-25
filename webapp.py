0e02ad9c2ff6c68c9b1c70fd4047acc32fa61d01
Alberto Clemente <165967398+crc137@users.noreply.github.com>
2026-09-25 15:12:00 +0000

    Add files via upload

--- webapp.py
+++ webapp.py
@@ -100,6 +100,42 @@ class Handler(BaseHTTPRequestHandler):
             self.send_header('Content-Length', str(len(body)))
             self.end_headers()
             self.wfile.write(body)
+        elif path.startswith('/img/'):
+            safe = os.path.basename(path)
+            fp = os.path.join(BASE, 'img', safe)
+            if os.path.isfile(fp):
+                body = open(fp, 'rb').read()
+                ctype = 'image/svg+xml' if safe.endswith('.svg') else ('image/jpeg' if safe.endswith('.jpg') else 'image/png')
+                self.send_response(200)
+                self.send_header('Content-Type', ctype)
+                self.send_header('Content-Length', str(len(body)))
+                self.end_headers()
+                self.wfile.write(body)
+            else:
+                self._json(404, {'ok': False})
+        elif path.startswith('/fonts/'):
+            safe = os.path.basename(path)
+            fp = os.path.join(BASE, 'fonts', safe)
+            if os.path.isfile(fp):
+                body = open(fp, 'rb').read()
+                if safe.endswith('.woff2'):
+                    ctype = 'font/woff2'
+                elif safe.endswith('.woff'):
+                    ctype = 'font/woff'
+                elif safe.endswith('.ttf'):
+                    ctype = 'font/ttf'
+                elif safe.endswith('.eot'):
+                    ctype = 'application/vnd.ms-fontobject'
+                else:
+                    ctype = 'application/octet-stream'
+                self.send_response(200)
+                self.send_header('Content-Type', ctype)
+                self.send_header('Content-Length', str(len(body)))
+                self.send_header('Cache-Control', 'max-age=86400')
+                self.end_headers()
+                self.wfile.write(body)
+            else:
+                self._json(404, {'ok': False})
         else:
             self._json(404, {'ok': False})
     def do_POST(self):
